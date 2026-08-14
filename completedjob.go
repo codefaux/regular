8d9ac910a2af91f9bf170f8730459bb1870262f3
codefaux <codefaux@solacenet.org>
2026-08-14 16:15:47 -0700

    Add *IsCode to completedjob, rewrite IsSuccess() to match

--- completedjob.go
+++ completedjob.go
@@ -7,12 +7,27 @@ import (
 type CompletedJob struct {
 	ExitMessage   string
 	ExitCode      int
+	ErrorIsCode   *int
+	WarningIsCode *int
+	SuccessIsCode *int
 	Started       time.Time
 	Finished      time.Time
 	WasForced     bool
 	AttachLogs    AttachMode
 }
 
-func (cj CompletedJob) IsSuccess() bool {
-	return cj.ExitCode == 0 && cj.ExitMessage == ""
+func (cj CompletedJob) ConsiderFailed() bool {
+	if (cj.SuccessIsCode != nil) && (cj.ExitCode != *cj.SuccessIsCode) {
+		return true
+	}
+
+	if (cj.ErrorIsCode != nil) && (cj.ExitCode == *cj.ErrorIsCode) {
+		return true
+	}
+
+	if (cj.SuccessIsCode == nil) && (cj.ErrorIsCode == nil) {
+		return cj.ExitCode == 0 && cj.ExitMessage == ""
+	} else {
+		return false
+	}
 }
